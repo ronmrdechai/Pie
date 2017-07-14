f00cb4f254deabec89a4130f9dc66f7c9ef5ec28
Ron Mordechai <ronmrdechai@gmail.com>
2017-07-14 15:32:48 +0300

    Added magic method comments

--- src/pie/object.h
+++ src/pie/object.h
@@ -19,6 +19,54 @@ public:
 
     object& operator=(object other);
 
+    // __repr__
+    // __str__
+    // __bytes__
+    // __lt__
+    // __le__
+    // __eq__
+    // __ne__
+    // __gt__
+    // __ge__
+    // __hash__
+    // __bool__
+
+    // __call__
+
+    // __len__
+    // __getitem__
+    // __setitem__
+    // __delitem__
+    // __contains__
+
+    // __add__
+    // __iadd__
+    // __sub__
+    // __isub__
+    // __mul__
+    // __imul__
+    // __truediv__
+    // __itruediv__
+    // __mod__
+    // __imod__
+    // __pow__
+    // __ipow__
+    // __lshift__
+    // __ilshift__
+    // __rshift__
+    // __irshift__
+    // __and__
+    // __iand__
+    // __xor__
+    // __ixor__
+    // __or__
+    // __ior__
+
+    // __neg__
+    // __pos__
+    // __abs__
+    // __invert__
+
 // Non-Python methods
 
     /// Access the underlying pointer
