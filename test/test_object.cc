2fd64f22e395ca7310837cc74ce203e1ecc3dcac
Ron Mordechai <ronmrdechai@gmail.com>
2017-07-14 13:45:35 +0300

    Added more tests

--- test/test_object.cc
+++ test/test_object.cc
@@ -29,3 +29,32 @@ TEST(object, copy_contructor) {
     EXPECT_EQ(refcount + 1, o->ob_refcnt);
     EXPECT_EQ(refcount + 1, copy->ob_refcnt);
 }
+
+TEST(object, move_contructor) {
+    object o(10);
+    ssize_t refcount = o->ob_refcnt;
+    object move(std::move(o));
+    EXPECT_EQ(nullptr, o.get());
+    EXPECT_EQ(refcount, move->ob_refcnt);
+}
+
+TEST(object, parse_assignment) {
+    object o = 10;
+    EXPECT_TRUE(PyNumber_Check(o.get()));
+}
+
+TEST(object, copy_assignment) {
+    object o(10);
+    ssize_t refcount = o->ob_refcnt;
+    object copy = o;
+    EXPECT_EQ(refcount + 1, o->ob_refcnt);
+    EXPECT_EQ(refcount + 1, copy->ob_refcnt);
+}
+
+TEST(object, move_assignment) {
+    object o(10);
+    ssize_t refcount = o->ob_refcnt;
+    object move = std::move(o);
+    EXPECT_EQ(nullptr, o.get());
+    EXPECT_EQ(refcount, move->ob_refcnt);
+}
