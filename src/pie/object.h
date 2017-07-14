3d194fbde8448d41010037e4e1860a3890dd67a5
Ron Mordechai <ronmrdechai@gmail.com>
2017-07-14 23:25:46 +0300

    Added prototypes for many functions

--- src/pie/object.h
+++ src/pie/object.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #include <Python.h>
+#include <ostream>
 
 namespace pie {
 
 class object {
+    struct proxy {
+        proxy(const object& o, const object& i);
+        proxy& operator=(const object& item);
+
+        const object& o;
+        const object& i;
+    };
+
 public:
     object();
     ~object();
@@ -17,27 +26,42 @@ public:
     object(object&& other);
     object(const object& other);
 
+    object(proxy other);
+
     object& operator=(object other);
 
     // __repr__
+    object repr() const;
     // __str__
     // __bytes__
+    object str() const;
     // __lt__
+    object operator< (const object& other) const;
     // __le__
+    object operator<=(const object& other) const;
     // __eq__
+    object operator==(const object& other) const;
     // __ne__
+    object operator!=(const object& other) const;
     // __gt__
+    object operator> (const object& other) const;
     // __ge__
+    object operator>=(const object& other) const;
     // __hash__
+    ssize_t hash() const;
     // __bool__
+    operator bool() const;
 
     // __call__
+    template <typename... Args>
+    object operator()(Args&&... args) const;
 
     // __len__
+    ssize_t length() const;
     // __getitem__
+    const proxy operator[](const object& item) const;
     // __setitem__
-    // __delitem__
-    // __contains__
+    proxy operator[](const object& item);
 
     // __add__
     // __iadd__
@@ -77,6 +101,12 @@ public:
 private:
     PyObject* m_obj;
 };
+
+// __getattr__
+// __setattr__
+// __hasattr__
+
+std::ostream& operator<<(std::ostream& os, const object& o);
 
 } // namespace pie
 
